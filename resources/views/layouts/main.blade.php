299053ed832092e22c038b5c8c294af5a36c2e0f
Joshua Weaver <jweaver@tolbookstores.com>
2017-03-06 16:05:54 -0500

    cleanup

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Soundboard.io</title>
+
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        @yield('content')
+    </div>
+</div>
+</body>
+</html>
