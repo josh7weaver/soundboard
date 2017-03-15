c63ccbe224f2a97b232698b94e6160d4ae6a6cba
Joshua Weaver <josh7weaver.work@gmail.com>
2017-03-14 23:54:01 -0400

    fixing response

--- app/Http/Controllers/SongApiController.php
+++ app/Http/Controllers/SongApiController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Song;
+use Illuminate\Http\Request;
 
 class SongApiController extends Controller
 {
-    public function songCommand()
+    public function songCommand(Request $request)
     {
         $song = Song::inRandomOrder()->first();
-        return $song;
+        return response()->json([
+          'response_type' => 'in_channel',
+          'text' => $song->youtube_url
+        ]);
     }
 }
