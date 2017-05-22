edbbd3313c334ae84b39c3a918a51583be4cae72
Joshua Weaver <jweaver@tolbookstores.com>
2017-05-22 10:57:04 -0400

    remove delete ability temporarily

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,15 +11,18 @@
 |
 */
 
-Route::get('/', ['uses' => 'SongController@index']);
+Route::get('/', 'BasicAuthController@loginForm');
+Route::post('/', 'BasicAuthController@actionLogin');
+
 Route::get('song/random', ['as' => 'song.random', 'uses' => 'SongController@random']);
 
 Route::resource('song', 'SongController');
 
-Route::get('song/{song}/delete', ['as' => 'song.delete', 'uses' => 'SongController@destroy']);
+//Route::get('song/{song}/delete', ['as' => 'song.delete', 'uses' => 'SongController@destroy']);
 
 Route::get('song/{song}/rating', ['as'=>'song.rating.edit', 'uses'=>'SongController@editRating']);
 Route::post('song/{song}/rating', ['as'=>'song.rating.update', 'uses'=>'SongController@updateRating']);
 
 
+// API
 Route::any('api/v1/song', ['as' => 'api.v1.song', 'uses' => 'SongApiController@songCommand']);
